686cccff0d2ec786e4f568c37aa7339c3ccd35a5
shakyasaijal <saijalshakya@gmail.com>
2020-10-19 12:29:35 +0545

    Feat:heroku setup

--- orderAndBilling/settings.py
+++ orderAndBilling/settings.py
@@ -99,11 +99,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media/images'),
 )
 
 GRAPHENE = {
