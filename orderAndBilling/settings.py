67a8e8fdab053618caa673174d495089fc630478
shakyasaijal <saijalshakya@gmail.com>
2020-10-19 12:19:01 +0545

    feat: heroku config

--- orderAndBilling/settings.py
+++ orderAndBilling/settings.py
@@ -101,8 +101,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = BASE_DIR+'/media'
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
